debff86a0dabc57d0d998152f3d2ec98776b4470
hiroyuki <ikuyorihato@gmail.com>
2016-07-13 20:21:45 +0000

    user-profile first commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710143342) do
+ActiveRecord::Schema.define(version: 20160712081034) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20160710143342) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "profile"
+    t.string   "location"
+    t.date     "birthday"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
